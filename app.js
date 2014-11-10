13b3e130c65bbc29f6f91b4262952648f9fe8368
Julio César Rodríguez Domínguez <jurasec@gmail.com>
2014-11-09 18:37:19 -0600

    ejemplo de five.Led()

--- app.js
+++ app.js
@@ -7,4 +7,14 @@ board = new five.Board();
 board.on('ready', function() {
   console.log('Board ready for fun!');
 
+  console.log(process.argv[0], process.argv[1]);
+
+  led = new five.Led({
+    pin: process.argv[2] || 13
+  });
+
+  this.repl.inject({
+    led: led
+  });
+
 });
